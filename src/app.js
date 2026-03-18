dcaf16184c067855243e22a46f8921b996c035b4
NickolasHardRock <nickolaslamin@gmail.com>
2026-03-17 21:50:09 -0300

    feat: initialize backend project with Express and basic structure
    - Added package.json for project metadata and dependencies
    - Created app.js to set up Express server
    - Added expense.json model for expense data structure
    - Created expense.js file for future expense-related functionalities

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const despasas = require('./models/expense.json');
+
+app.get('/',(req,res)=>{
+    res.
+})
+
+const PORT = 3000;
+
+app.listen(PORT,() => {
+    console.log("Servidor foi iniciado na porta:", PORT);
+});
+
