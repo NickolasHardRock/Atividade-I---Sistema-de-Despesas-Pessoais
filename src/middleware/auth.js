09518da246210f7a42bf2b5f7f221c423b5954f4
NickolasHardRock <nickolaslamin@gmail.com>
2026-06-02 21:38:32 -0300

    feat: add JWT authentication middleware and configuration using dotenv

---
+++ src/middleware/auth.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import authConfig from '../config/auth.js';
+
+function authMiddleware(req,res,next){
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader){
+            return res.status(401).json({error: 'Token não informado'});
+        }
+
+        const [scheme, token] = authHeader.split(' ');
+
+        if(scheme !== 'Bearer' || !token){
+            return res.status(401).json({error:'Token mal formatado'});
+        }
+
+        try{
+            const  decod = jwt.verify(token,authConfig.jwt.secret);
+            req.user = {
+                id: decod.id,
+                email: decod.email,
+                role: decod.role
+            };
+
+            const method = req.method;
+            const path = req.path;
+
+            console.log(`Middleware de autenticação: ${method} ${path} - Usuário: ${req.user.email} (Role: ${req.user.role})`);
+            if(path.startsWith('/usuarios'))
+
+
+        }
+
+
+
+    }
+
+    
+
